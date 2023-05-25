e1dc24da53c6c094754da3426eb0bb4357b17e97
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-25 12:11:23 +0900

    up

--- plot_smoothing.cpp
+++ plot_smoothing.cpp
@@ -1,13 +1,15 @@
 #include "plot_smoothing.hpp"
 
 
-void PlotSmoothing::Smoothing(float* plot)
+void PlotSmoothing::Smoothing(float *plot)
 {
     float buff[SAMPLE_NUM] = {0};
     float stack = 0;
 
     for(int i = 0; i < SAMPLE_NUM; i++)
     {
+        buff 
+        stack += 
 
     }
 
