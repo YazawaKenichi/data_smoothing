032d2d0720bcd3810e3c0b9717b9c02cbc82f50d
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-27 02:17:41 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include "main.hpp"
 
+#define DATA_SIZE 100
+
+
+void PlotSmoothing::Run()
+{
+    float plot_data[DATA_SIZE] = {0};
+
+    for{}
+
+
+
+    Smoothing()
+    Print()
+
+}
+
+void PlotSmoothing::Print(float *plot)
+{
+    printf("plot = %f", plot);
+}
+
 int main()
 {
 
--- plot_smoothing.cpp
+++ plot_smoothing.cpp
@@ -1,35 +1,29 @@
 #include "plot_smoothing.hpp"
 
-void PlotSmoothing::Smoothing(float *plot)
-{
-    float buff[SAMPLE_NUM] = {0};
-    float stack = 0;
-
-    plot = 55.55;
-}
-
-void PlotSmoothing::Print(float *plot)
-{
-    printf("plot = %f", plot);
-}
-
-void PlotSmoothing::Run()
-{
-
-
-
-    Smoothing()
-    Print()
-
-}
-
-void PlotSmoothing::Smoothing(float *data_addr, uint8_t buff_size, uint16_t data_size)
+void Smoothing(float *data_addr, uint16_t num_of_adjacent, uint16_t data_size)
 {
     float cp_data[data_size] = {0};
+    uint16_t i, j;
+    int16_t min, max;
+    float avg;
+    double sum;
 
-}
+    for(i = 0; i < size; i++)
+    {
+        sum = 0;
+        min = i - num_of_adjacent;
+        max = i + num_of_adjacent;
+        for(j = min; j <= max; j++)
+        {
+            if(j < 0 || j >= size) continue;
+            sum += *(data_addr + j);
+        }
+        avg = (float)sum / (num_of_adjacent * 2 + 1);
+        cp_data[i] = avg;
+    }
 
-void PlotSmoothing::Average()
-{
-
+    for(i = 0; i < size; i++)
+    {
+        *(data_addr + i) = cp_data[i];
+    }
 }
