66fefd7eff70ec036d445eb9f4481b284ea91919
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-27 01:41:15 +0900

    update

--- plot_smoothing.cpp
+++ plot_smoothing.cpp
@@ -1,6 +1,5 @@
 #include "plot_smoothing.hpp"
 
-
 void PlotSmoothing::Smoothing(float *plot)
 {
     float buff[SAMPLE_NUM] = {0};
@@ -12,4 +11,25 @@ void PlotSmoothing::Smoothing(float *plot)
 void PlotSmoothing::Print(float *plot)
 {
     printf("plot = %f", plot);
+}
+
+void PlotSmoothing::Run()
+{
+
+
+
+    Smoothing()
+    Print()
+
+}
+
+void PlotSmoothing::Smoothing(float *data_addr, uint8_t buff_size, uint16_t data_size)
+{
+    float cp_data[data_size] = {0};
+
+}
+
+void PlotSmoothing::Average()
+{
+
 }
